158a36cac93dcb66fddd565de3d8545c7dcc7093
prashansa <joshiprashansa11@gmail.com>
2024-06-18 17:23:41 +0545

    Added the terraform configuration

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,7 @@
 terraform {
   backend "s3" {
     region = "us-east-1"
-    key    = "426857564226/prashansa"
+    key    = "426857564226/prashansa_cicd_node.tfstate"
     bucket = "8586-terraform-state"
   }
 }
-  
